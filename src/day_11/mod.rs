a5a0d62540e0e0991ed0335a6cb41c28d9e8edf3
Aleksander Salek <asa@limessecurity.com>
2024-12-11 22:36:28 +0100

    cleanup day 11

--- src/day_11/mod.rs
+++ src/day_11/mod.rs
@@ -3,23 +3,27 @@ use memoize::memoize;
 pub fn run_day_11() {
     let numbers: Vec<u128> = vec![17639, 47, 3858, 0, 470624, 9467423, 5, 188];
     // We start sum from the length of the numbers (as in your original code)
-    let mut sum = numbers.len() as u128;
-    for number in numbers {
-        // Start iteration at 0 for each number
-        sum += solve_part_01(number, 0);
+    let mut sum_part_01 = numbers.len() as u128;
+    let mut sum_part_02 = numbers.len() as u128;
+
+    for number in numbers.clone() {
+        sum_part_01 += solve(number, 0, 25);
+        sum_part_02 += solve(number, 0, 75);
     }
-    println!("part one: {}", sum);
+
+    println!("part one: {}", sum_part_01);
+    println!("part two: {}", sum_part_02);
 }
 
 #[memoize]
-fn solve_part_01(number: u128, iteration: u128) -> u128 {
-    if iteration >= 75 {
+fn solve(number: u128, iteration: u16, max_depth: u16) -> u128 {
+    if iteration >= max_depth {
         return 0;
     }
     let iter = iteration + 1;
 
     if number == 0 {
-        return solve_part_01(1, iter);
+        return solve(1, iter, max_depth);
     }
 
     let num_str = number.to_string();
@@ -29,16 +33,16 @@ fn solve_part_01(number: u128, iteration: u128) -> u128 {
 
         let left_half = &num_str[..num_str.len() / 2];
         let left_half_val = left_half.parse::<u128>().unwrap();
-        total += solve_part_01(left_half_val, iter);
+        total += solve(left_half_val, iter, max_depth);
 
         let right_half = &num_str[num_str.len() / 2..];
         let right_half_val = right_half.parse::<u128>().unwrap();
-        total += solve_part_01(right_half_val, iter);
+        total += solve(right_half_val, iter, max_depth);
 
-        total
-    } else {
-        // If length is odd, multiply by 2024 and recurse
-        let mul_1024 = number * 2024;
-        solve_part_01(mul_1024, iter)
+        return total;
     }
+
+    // If length is odd, multiply by 2024 and recurse
+    let mul_1024 = number * 2024;
+    solve(mul_1024, iter, max_depth)
 }
